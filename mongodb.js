89059c4fc086643073b70327f3728e62923fa856
unknown <sabatevdorashvili99@gmail.com>
2021-12-22 01:03:54 -0800

    inserting documents to the database

--- mongodb.js
+++ mongodb.js
@@ -2,20 +2,58 @@
 
 const mongodb = require("mongodb");
 
-const MongoClient = mongodb.MongoClient
+const MongoClient = mongodb.MongoClient;
 
-const connectionUrl = "mongodb://127.0.0.1:27017"
-const databaseName = "task-manager"
+const connectionUrl = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
 
-MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(
+  connectionUrl,
+  { useNewUrlParser: true },
+  (error, client) => {
     if (error) {
-       return  console.log("Unable to connect to databse")
-    } 
-    console.log("Connected to database!")
+      return console.log("Unable to connect to databse");
+    }
+    console.log("Connected to database!");
 
-    const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        name:"Saba",
-        age:22
-    })
-})
+    const db = client.db(databaseName);
+    // db.collection('users').insertMany({
+    //     name:"Saba",
+    //     age:23
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log("Unable to connect to database")
+    //     }
+    //     console.log(result.insertedId)
+    // })
+    // db.collection('users').insertMany([
+    //     {
+    //         name:"Saba",
+    //         age:24
+    //     },
+    //     {
+    //         name:"Saba",
+    //         age:25
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log(error)
+    //     }
+    //     console.log(result.insertedIds)
+    // })
+
+    db.collection("tasks").insertMany(
+      [
+        { descirption: "Study node", completed: false },
+        { descirption: "Study Angular", completed: false },
+        { descirption: "Work out", completed: true },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to connect to the database");
+        }
+        console.log(result.insertedIds);
+      }
+    );
+  }
+);
