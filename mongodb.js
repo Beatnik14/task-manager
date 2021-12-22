69e744cdee8bc61316629eb246b0702be083bfa7
unknown <sabatevdorashvili99@gmail.com>
2021-12-22 04:35:29 -0800

    code cleaned

--- mongodb.js
+++ mongodb.js
@@ -13,54 +13,23 @@ MongoClient.connect(
       return console.log("Unable to connect to the database");
     }
     const db = client.db(databaseName);
-   
-   db.collection("tasks").find({completed: false}).toArray((error, tasks) => {
-    if (error) {
-      return console.log("Error occured")
-    } 
-    console.log(tasks)
-   })
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-    // db.collection("tasks").findOne({_id: new ObjectId("61c2e537db10aaa85308843f")}, (error, result) => {
-    //   if (error) {
-    //     return console.log("Error occured")
-    //   }
-    //   console.log(result)
-    // })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //   db.collection("users")
-  //     .find({
-  //       age: 22,
-  //     })
-  //     .toArray((error, users) => {
-  //       if (error) {
-  //         return console.log("Error");
-  //       }
-  //       console.log(users);
-  //     });
-   }
+    db.collection("tasks")
+      .find({ completed: false })
+      .toArray((error, tasks) => {
+        if (error) {
+          return console.log("Error occured");
+        }
+        console.log(tasks);
+      });
+    db.collection("tasks").findOne(
+      { _id: new ObjectId("61c2e537db10aaa85308843f") },
+      (error, result) => {
+        if (error) {
+          return console.log("Error occured");
+        }
+        console.log(result);
+      }
+    );
+  }
 );
