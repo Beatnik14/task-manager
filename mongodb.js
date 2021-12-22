bb892efac1a75952f2cd97bff6e843db27f487a6
unknown <sabatevdorashvili99@gmail.com>
2021-12-22 04:32:21 -0800

    finding documents in mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,13 +1,6 @@
 //CRUD create read update delete
 
-// const mongodb = require("mongodb");
-// const MongoClient = mongodb.MongoClient;
-// const ObjectId = mongodb.ObjectId;
-
 const { MongoClient, ObjectId } = require("mongodb");
-const id = new ObjectId();
-console.log(id);
-console.log(id.getTimestamp());
 
 const connectionUrl = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
@@ -17,49 +10,57 @@ MongoClient.connect(
   { useNewUrlParser: true },
   (error, client) => {
     if (error) {
-      return console.log("Unable to connect to databse");
+      return console.log("Unable to connect to the database");
     }
-
     const db = client.db(databaseName);
-    db.collection('users').insertMany({
-        _id: id,
-        name:"Saba",
-        age:23
-    }, (error, result) => {
-        if (error) {
-            return console.log("Unable to connect to database")
-        }
-        console.log(result.insertedId)
-    })
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name:"Saba",
-    //         age:24
-    //     },
-    //     {
-    //         name:"Saba",
-    //         age:25
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log(error)
-    //     }
-    //     console.log(result.insertedIds)
+   
+   db.collection("tasks").find({completed: false}).toArray((error, tasks) => {
+    if (error) {
+      return console.log("Error occured")
+    } 
+    console.log(tasks)
+   })
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    // db.collection("tasks").findOne({_id: new ObjectId("61c2e537db10aaa85308843f")}, (error, result) => {
+    //   if (error) {
+    //     return console.log("Error occured")
+    //   }
+    //   console.log(result)
     // })
 
-    // db.collection("tasks").insertMany(
-    //   [
-    //     { descirption: "Study node", completed: false },
-    //     { descirption: "Study Angular", completed: false },
-    //     { descirption: "Work out", completed: true },
-    //   ],
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to connect to the database");
-    //     }
-    //     console.log(result.insertedIds);
-    //   }
-    // );
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //   db.collection("users")
+  //     .find({
+  //       age: 22,
+  //     })
+  //     .toArray((error, users) => {
+  //       if (error) {
+  //         return console.log("Error");
+  //       }
+  //       console.log(users);
+  //     });
+   }
 );
