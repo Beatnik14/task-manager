22aa01dcacb54c70cd835a837359cd3e5e0ff30e
unknown <sabatevdorashvili99@gmail.com>
2021-12-23 08:59:42 -0800

    mongoose added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,24 @@
 const mongoose = require("mongoose");
 
-const connectionUrl = "mongodb://127.0.0.1:27017/task-manager-api"
+const connectionUrl = "mongodb://127.0.0.1:27017/task-manager-api";
 
-mongoose.connect(connectionUrl, {useNewUrlParser: true, useCreateIndex: true})
+mongoose.connect(connectionUrl);
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+});
+
+const me = new User({ name: "Saba", age: 22 });
+
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((error) => {
+    console.log("Error", error);
+  });
