85d33488e2f33f8006c5c2617c5690cf82c4eeb2
unknown <sabatevdorashvili99@gmail.com>
2021-12-27 01:57:22 -0800

    validation added to the mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,17 +1,80 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const connectionUrl = "mongodb://127.0.0.1:27017/task-manager-api";
 mongoose.connect(connectionUrl);
 
 const Task = mongoose.model("Task", {
-  description: {type:String},
-  completed: {type: Boolean}
-})
+  description: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+  },
+  email: {
+    type: String,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Please provide valid email");
+      }
+    },
+  },
+});
 
-const node = new Task({description:"Study Node", completed: false});
+const node = new Task({
+  description: "Study node",
+  completed: false,
+  email: "sabatevdorashvili99@gmail.com",
+});
 
-node.save().then((result) => {
-  console.log(result)
-}).catch((error) => {
-  console.log(error)
-})
+// node
+//   .save()
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Please provide valid email");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be greater than zero");
+      }
+    },
+  },
+});
+
+const me = new User({
+  name: "Saba",
+  email: "SABATEVDORASHVILI99@gmail.com",
+  age: 22,
+});
+
+me.save()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
