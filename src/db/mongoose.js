fe6861f30b29debc0949cb871854a08a0eec71a0
unknown <sabatevdorashvili99@gmail.com>
2021-12-27 01:04:49 -0800

    created model task

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,24 +1,70 @@
 const mongoose = require("mongoose");
 
 const connectionUrl = "mongodb://127.0.0.1:27017/task-manager-api";
-
 mongoose.connect(connectionUrl);
 
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-});
+const Task = mongoose.model("Task", {
+  description: {type:String},
+  completed: {type: Boolean}
+})
 
-const me = new User({ name: "Saba", age: 24 });
+const node = new Task({description:"Study Node", completed: false});
 
-me.save()
-  .then(() => {
-    console.log(me);
-  })
-  .catch((error) => {
-    console.log("Error", error);
-  });
+node.save().then((result) => {
+  console.log(result)
+}).catch((error) => {
+  console.log(error)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require("mongoose");
+
+// const connectionUrl = "mongodb://127.0.0.1:27017/task-manager-api";
+
+// mongoose.connect(connectionUrl);
+
+// const User = mongoose.model("User", {
+//   name: {
+//     type: String,
+//   },
+//   age: {
+//     type: Number,
+//   },
+// });
+
+// const me = new User({ name: "Saba", age: 24 });
+
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch((error) => {
+//     console.log("Error", error);
+//   });
