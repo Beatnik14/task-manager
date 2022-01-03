87f9c8996848fd64045203f74f3a2d9090f23374
unknown <sabatevdorashvili99@gmail.com>
2022-01-03 08:36:51 -0800

    added express

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 const connectionUrl = "mongodb://127.0.0.1:27017/task-manager-api";
 mongoose.connect(connectionUrl);
@@ -8,73 +7,11 @@ const Task = mongoose.model("Task", {
   description: {
     type: String,
     required: true,
+    trim: true
   },
   completed: {
     type: Boolean,
-  },
-  email: {
-    type: String,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Please provide valid email");
-      }
-    },
-  },
+    default: false
+  }
 });
 
-const node = new Task({
-  description: "Study node",
-  completed: false,
-  email: "sabatevdorashvili99@gmail.com",
-});
-
-// node
-//   .save()
-//   .then((result) => {
-//     console.log(result);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Please provide valid email");
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be greater than zero");
-      }
-    },
-  },
-});
-
-const me = new User({
-  name: "Saba",
-  email: "SABATEVDORASHVILI99@gmail.com",
-  age: 22,
-});
-
-me.save()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
---
+++ src/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Please provide valid email");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be greater than zero");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.includes("password")) {
+        throw new Error(
+          'Password should contain more than 6 symbols and it mustn\'t be a "password"'
+        );
+      }
+    },
+  },
+});
+
+module.exports = User;
