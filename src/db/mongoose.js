ae584dbd1569efd730f5d0e219c81640174ba45d
unknown <sabatevdorashvili99@gmail.com>
2022-01-05 10:00:38 -0800

    creating endpoint

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,15 +3,4 @@ const mongoose = require("mongoose");
 const connectionUrl = "mongodb://127.0.0.1:27017/task-manager-api";
 mongoose.connect(connectionUrl);
 
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
-});
 
